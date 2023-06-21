8c7256cb2d58ac66c94cc0dea8ccb4b8ac37f5c3
SGzhibo <1905471686@qq.com>
2023-06-22 00:25:48 +0800

    配置了基本插件，颜色主题，侧边文件浏览器

---
+++ init.lua
@@ -0,0 +1,20 @@
+--基础设置
+
+--require 函数在 Lua 中用于加载一个模块，
+--而这些模块通常位于 runtimepath 中的 lua/ 目录下，也就是我们的 ~/.config/nvim/lua/ 目录。
+--所以上边的代码，就是加载 ~/.config/nvim/lua/basic.lua 文件（注意：require 里没有 .lua 扩展名）。
+--当然也可以创建 ~/.config/nvim/lua/basic/ 目录，在目录下边创建 init.lua 文件也是可以成功加载的。
+require('basic')
+
+-- 快捷键映射
+require("keybindings")
+
+-- Packer 插件管理
+require("plugins")
+
+-- 主题设置 （新增）
+require("colorscheme")
+
+-- 插件配置
+require("plugin-config.nvim-tree")
+
---
+++ lua/plugins.lua
@@ -0,0 +1,27 @@
+local packer = require("packer")
+packer.startup(
+  function(use)
+   -- Packer 可以管理自己本身
+   use 'wbthomason/packer.nvim'
+   -------------------------- plugins -------------------------------------------
+   -- nvim-tree (新增)
+   use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
+
+   --------------------- colorschemes --------------------
+   -- tokyonight
+   use("folke/tokyonight.nvim")
+   -------------------------------------------------------
+
+ end)
+
+-- 每次保存 plugins.lua 自动安装插件
+pcall(
+  vim.cmd,
+  [[
+    augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    augroup end
+  ]]
+)
+
